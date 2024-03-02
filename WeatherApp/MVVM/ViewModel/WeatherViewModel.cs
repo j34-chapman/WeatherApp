7b812545d47474072a44f31dda7f26b1da5320da
j34-chapman <jakechapman710@gmail.com>
2024-03-02 13:39:02 +0000

    Initial Commit

---
+++ WeatherApp/MVVM/ViewModel/WeatherViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherApp.MVVM.ViewModel;
+
+public class WeatherViewModel : ContentPage
+{
+	public WeatherViewModel()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
---
+++ WeatherApp/MVVM/Views/WeatherView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using WeatherApp.MVVM.ViewModel;
+
+namespace WeatherApp.MVVM.Views;
+
+public partial class WeatherView : ContentPage
+{
+	public WeatherView()
+	{
+		InitializeComponent();
+		BindingContext = new WeatherViewModel();
+	}
+}
